23101994b66b04f4278692521ac37e9c9fd05aeb
David Agustin <davidsyagustin@gmail.com>
2019-03-05 01:14:28 -0800

    Finish isUnique

---
+++ 1.1 isUnique.js
@@ -0,0 +1,53 @@
+/*
+Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional
+data structures?
+ */
+
+const isUnique = (string) => {
+  if (string === '') return false;
+  // create set
+  const uniqueCharacters = new Set;
+  // iterate through string
+  for (let i = 0; i < string.length; i++) {
+    // add character to set
+    uniqueCharacters.add(string[i]);
+  }
+  // conditional statement of the lengths of set and string
+  return [...uniqueCharacters].length === string.length;
+};
+
+// optimization
+const isUniqueOptimized = (string) => {
+  for (let i = 0; i < string.length; i++) {
+    if (string.indexOf(string[i]) !== string.lastIndexOf(string[i])) {
+      return false
+    }
+  }
+  return true
+};
+
+// 'abc' => true
+// 'aabc' => false
+// '' => false
+
+// test cases
+const argument = 'aabc';
+const actual = isUniqueOptimized(argument);
+const expected = false;
+const testCase = 'Determines if string has unique characters';
+
+const assertEquals = (input, output, testCase) => {
+  if (input !== output) {
+    return `ERROR ${testCase}: Expected ${output} but got ${input}`
+  }
+  return 'Passed';
+};
+
+const answer = assertEquals(actual, expected, testCase);
+
+console.log(answer);
+
+// Notes ask if string is in ASCII or Unicode
+
+
+
---
+++ README.md
@@ -0,0 +1,11 @@
+Walking through a problem:
+
+1. Listen
+2. Example
+3. Brute Force
+4. Optimize
+5. Walk Through
+6. Implement
+7. Test
+
+(BUD Optimization: Bottlenecks, Unnecessary Work, Duplicated Work)
